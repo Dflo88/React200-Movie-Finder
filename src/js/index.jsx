56ea8097409bbd881461ff1a584ac5bbfd81c670
Diego <dflores7237@gmail.com>
2020-01-17 16:43:21 -0800

    set up files to make initial router work

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import reducers from './reducers';
+// import { createStore } from 'redux';
+// import reducers from './reducers';
 import App from './app';
+import store from './rootStore';
 
 /* eslint-disable no-underscore-dangle */
-const store = createStore(
-  reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+// const store = createStore(
+//   reducers,
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
 /* eslint-enable */
 
 
